86f02446d88378feb922df6d22261a582e64861b
Kai Morris <kaij.morris@yahoo.com>
2022-10-18 15:47:25 -0400

    finished profile

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Name: Kai
+            //Favorite Color: Blue
+            //Favorite Animal: Lion
+            //Favorite Music Artist: Jay-Z
+
+            Console.WriteLine("Hello - What is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hey {userName}, What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"That's cool {userName}, mines is too. What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"Noicee! And who's your favorite artist?");
+            var artist = Console.ReadLine();
+
+
+
+            Console.WriteLine($"Cool {userName}, Here is your profile");
+
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Color: {color}");
+            Console.WriteLine($"Animal: {animal}");
+            Console.WriteLine($"Artist: {artist}");
+
         }
     }
 }
